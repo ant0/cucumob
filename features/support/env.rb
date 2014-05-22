8d4db284346ff600035ac3444a9e7cd2a58f12bd
Anthony Low <anthony.low@xbridge.com>
2014-05-22 16:57:17 +0100

    Update env.rb
    using capybara

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,3 +20,32 @@ def selenium
 end
 
 After { selenium.quit }
+
+#require 'selenium-webdriver'
+#require 'capybara'
+#require 'capybara/cucumber'
+#
+#
+#def driver_capabilities
+#  {
+#    :device      => 'iPhone',
+#    :browserName => 'Safari iOS',
+#    :version     => '7.1',
+#    :platform    => 'Mac',
+#    :app         => 'Safari'
+#  }
+#end
+#
+#def server_url
+#  "http://127.0.0.1:4723/wd/hub"
+#end
+#
+#Capybara.register_driver :iOS do |app|
+#  Capybara::Selenium::Driver.new(app,
+#    browser: :remote,
+#    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.safari(driver_capabilities),
+#    url: server_url)
+#end
+#
+#Capybara.default_driver = :iOS
+#
